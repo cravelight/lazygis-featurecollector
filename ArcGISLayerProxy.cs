d78e1333ece8262637bef4f9db663d8ff69afb08
Mark Gidman <cravelight@gmail.com>
2016-01-21 19:43:58 -0600

    updated to get features in groups of 500 instead of one at a time

--- ArcGISLayerProxy.cs
+++ ArcGISLayerProxy.cs
@@ -71,11 +71,63 @@ namespace LazyGIS.FeatureCollector
                 geometryType = _layerDetails.geometryType,
                 spatialReference = _layerDetails.extent["spatialReference"],
                 fieldAliases = _layerDetails.fields.ToDictionary<dynamic, string, string>(field => field["alias"], field => field["name"]),
-                features = ObjectIds.Select(id => GetFeatureForObject(id)).ToList()
+                features = GetFeaturesForLayer()
             };
 
             return dto;
         }
+
+        private List<dynamic> GetFeaturesForLayer()
+        {
+            var allFeatures = new List<dynamic>();
+
+            ObjectIds.Sort(); // make sure ids are in numerical order
+
+            /* cycle through the object ids in groups and add the range
+             * 
+             * Why 500? The ArcGIS servers will only return data for 1000 records at a time.
+             * 
+             * I tried a variety of numbers and found that chunks less than 500 tended to be
+             * slow because of too much internet overhead.  The overall time for ~9000 records
+             * was the same when using chunks of either 500 or 1000. Given that neither made a 
+             * big difference in performance I chose the smaller number to decrease the risk of
+             * a slow connection timing out with the bigger payload you get with chunks of 1000.
+             */
+            var objectIdLists = ObjectIds.ChunkBy(500); // break the object list into groups of 500
+            foreach (var list in objectIdLists)
+            {
+                var lower = list.Min() - 1;
+                var upper = list.Max() + 1;
+                allFeatures.AddRange(GetFeaturesInRange(lower, upper));
+            }
+
+            return allFeatures;
+        }
+
+        // does not include start/end ids
+        private List<dynamic> GetFeaturesInRange(int startObjectId, int endObjectId)
+        {
+            using (var webClient = new System.Net.WebClient())
+            {
+                var json = webClient.DownloadString(GetFeatureDetailsCollectionUrl(startObjectId, endObjectId));
+                var dto = JsonConvert.DeserializeObject<ArcGISFeatureDetailsCollectionDto>(json);
+                return dto.features;
+            }
+        }
+
+        // does not include start/end ids
+        private string GetFeatureDetailsCollectionUrl(int startObjectId, int endObjectId)
+        {
+            const string template = "{baseurl}/query?where=objectid+%3E+{startId}+and+objectid+%3C+{endId}&returnGeometry=true&outFields=*&f=pjson";
+            var url = template.Replace("{baseurl}", _baseUrl);
+            url = url.Replace("{startId}", startObjectId.ToString());
+            return url.Replace("{endId}", endObjectId.ToString());
+        }
+
+
+        #region worked but slow
+
+        //ObjectIds.Select(id => GetFeatureForObject(id)).ToList()
 
         // this worked but getting them one at a time took forever
         private dynamic GetFeatureForObject(int objectId)
@@ -85,16 +137,18 @@ namespace LazyGIS.FeatureCollector
                 var json = webClient.DownloadString(GetFeatureDetailsUrl(objectId));
                 var dto = JsonConvert.DeserializeObject<ArcGISFeatureDetailsDto>(json);
                 return dto.feature;
-
             }
         }
 
+        // this worked but getting them one at a time took forever
         private string GetFeatureDetailsUrl(int objectId)
         {
             const string template = "{baseurl}/{id}?f=pjson";
             var url = template.Replace("{baseurl}", _baseUrl);
             return url.Replace("{id}", objectId.ToString());
         }
+        
+        #endregion // worked but slow
 
         #endregion // ArcGISFeatureSet builder
 
@@ -144,4 +198,22 @@ namespace LazyGIS.FeatureCollector
         #endregion //Object ID Collection
 
     }
+
+
+    // HT: http://stackoverflow.com/a/24087164
+    /// <summary>
+    /// Helper methods for the lists.
+    /// </summary>
+    public static class ListExtensions
+    {
+        public static List<List<T>> ChunkBy<T>(this List<T> source, int chunkSize)
+        {
+            return source
+                .Select((x, i) => new { Index = i, Value = x })
+                .GroupBy(x => x.Index / chunkSize)
+                .Select(x => x.Select(v => v.Value).ToList())
+                .ToList();
+        }
+    }
+
 }
---
+++ Dto/ArcGISFeatureDetailsCollectionDto.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace LazyGIS.FeatureCollector.Dto
+{
+    /// <summary>
+    /// deserialization template for feature details collection json
+    /// all we need is the features, so we don't deserialize anything but the collection
+    /// "get features where objectid &gt; 0 and objectid &lt; 10" 
+    /// Sample: http://ww1.bucoks.com/arcgis/rest/services/BuCoKs_NGStreets/MapServer/0/query?where=objectid+%3E+0+and+objectid+%3C+10&returnGeometry=true&outFields=*&f=pjson
+    /// </summary>
+    public class ArcGISFeatureDetailsCollectionDto
+    {
+        public List<dynamic> features { get; set; }
+    }
+}
